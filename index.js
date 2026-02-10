a5a613ea14d14cca11ab5e50b3b02876849895e0
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-10 11:28:56 -0500

    Finished Trampoline

--- index.js
+++ index.js
@@ -7,8 +7,33 @@ function countUp(x){
 }
 
 try{
-    countUp(counter);
+   // countUp(counter);
 } catch(Error){
-    console.log("Stackoverflow achieved!", counter, Error)
+   // console.log("Stackoverflow achieved!", counter, Error)
 } //WORKS IN CONSOLE INSIDE BROSWER, DOESN'T INSIDE VSCODE
+
+
+
+function store(func){
+    let result=func();
+    while(typeof result=="function"){
+        result=result();
+    }
+    return result;
+}
+
+
+counter = 0;
+function trampoline(x){
+    console.log(x);
+    x++;
+    return function(){
+        return trampoline(x);
+    }
+    
+
+}
+store(trampoline(counter))
+
+
 
