1cbea0bf29e5d0d05a41bcf4fee53b6b6fcb329a
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-10 14:49:11 -0500

    Got base prime detection and printing working, needed to add HTML basic user input and timeOut

--- index.js
+++ index.js
@@ -33,7 +33,64 @@ function trampoline(x){
     
 
 }
-store(trampoline(counter))
+//store(trampoline(counter)) <- This causes an infinite loop
+//Work on this still
 
+let pElement = document.querySelector("p")
+let number = Number(pElement.textContent);
+console.log(number+4);
 
+function primeNumber(n){
+    let count =0;
+    let primeCount = [];
+    //pElement.innerHTML+= "<li>This is a test</li>"
+    let isPrime = false;
 
+    
+    for(let i = 1; i<n+1; i++){ 
+        if(n%i===0){
+            primeCount.push(true);
+            //primeNumber(i);
+            console.log(i, "is divisible.")
+
+        } else{
+            primeCount.push(false);
+            //primeNumber(i);
+            console.log(i, "not divisble.")
+        }
+    }
+    if(n===1){
+        return {num: n, prime: false} ;   
+    }
+    primeCount.forEach(function(item){
+        if(item===true){
+            count=count+1
+        }
+    })
+    if(count===2){
+        console.log("It's a prime!")
+
+        return {num: n, prime: true};
+    } else if(count>2)
+    {console.log("Not a prime."); return {num: n, prime: false};}
+   // if(primeCount.forEach.true === 2){
+      //  console.log("test");
+   // }
+
+}
+
+function printPrime(n){
+
+    for (let y=n; y>1; y--){
+       // primeResult = primeNumber(y)
+        if(primeNumber(y).prime===true){
+
+            pElement.innerHTML+=`<li> ${primeNumber(y).num} </li>`;
+            
+        }
+
+    }
+}
+printPrime(97);
+alert("Initial Phase finished! Now starting 10,000!");
+printPrime(700);
