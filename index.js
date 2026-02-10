1fbddc1ccc1d48fdf2ffa1a9c402691c0eea3a24
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-10 10:40:17 -0500

    Test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let counter = 0;
+
+function countUp(x){
+    console.log(x);
+    x++
+    countUp(x);
+}
+
+try{
+    countUp(counter);
+} catch(Error){
+    console.log("Stackoverflow achieved!", counter, Error)
+} //WORKS IN CONSOLE INSIDE BROSWER, DOESN'T INSIDE VSCODE
 
